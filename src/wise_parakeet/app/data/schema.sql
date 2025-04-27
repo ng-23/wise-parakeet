006b741f30bf9188c5f39e8e5fe6fabd586ad55b
ng-23 <23grattan@gmail.com>
2025-04-27 15:53:16 -0400

    Major changes
    Added tables for storing vocabularies and mapping models to the vocabs they were fit on.

--- src/wise_parakeet/app/data/schema.sql
+++ src/wise_parakeet/app/data/schema.sql
@@ -3,5 +3,20 @@ CREATE TABLE IF NOT EXISTS models (
     created_at INTEGER NOT NULL,
     typ TEXT NOT NULL,
     threshold FLOAT NOT NULL,
-    path_to TEXT NOT NULL
+    pkl_pth TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS vocabs (
+    id INTEGER not NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
+    created_at INTEGER NOT NULL,
+    pkl_pth TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS model_vocabs (
+    model_id INTEGER NOT NULL UNIQUE,
+    vocab_id INTEGER NOT NULL,
+    PRIMARY KEY(model_id, vocab_id),
+    FOREIGN KEY(model_id) REFERENCES models(id),
+    FOREIGN KEY(vocab_id) REFERENCES vocabs(id)
+);
+
